3a3405051ac55339b46b11826b4171cd18223863
klamaj <kri.lamaj@gmail.com>
2024-07-24 17:28:25 +0300

    Terms Pages Components + Routing

--- src/app/modules/pages/pages.module.ts
+++ src/app/modules/pages/pages.module.ts
@@ -1,15 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact/contact.component';
+import { TermsOfUseComponent } from './terms-of-use/terms-of-use.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { CookiesPolicyComponent } from './cookies-policy/cookies-policy.component';
 
-
+export const routes: Routes = [
+  {
+    path: 'terms-of-use',
+    component: TermsOfUseComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'privacy-policy',
+    component: PrivacyPolicyComponent
+  },
+  {
+    path: 'cookies-policy',
+    component: CookiesPolicyComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    ContactComponent
+    ContactComponent,
+    TermsOfUseComponent,
+    PrivacyPolicyComponent,
+    CookiesPolicyComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class PagesModule { }
