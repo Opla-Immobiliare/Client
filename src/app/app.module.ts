ffa14412901968c85b3760c9e5fc6de734cce99c
klamaj <kri.lamaj@gmail.com>
2024-08-28 22:36:42 +0300

    Install and SetUp @NgRx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+
+import { StoreModule } from '@ngrx/store';
+import { metaReducers, reducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { EntityDataModule } from '@ngrx/data';
+import { RouterState, StoreRouterConnectingModule } from '@ngrx/router-store';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/pages/pages.module').then(m => m.PagesModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
+  }
+];
 
 @NgModule({
   declarations: [
@@ -11,8 +31,28 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    CoreModule
+    CoreModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes, {}),
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictActionImmutability: true,
+        strictStateImmutability: true,
+        strictActionSerializability: true,
+        strictStateSerializability: true,
+      }
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: !environment.production
+    }),
+    EffectsModule.forRoot([]),
+    EntityDataModule.forRoot({}),
+    StoreRouterConnectingModule.forRoot({
+      stateKey: 'router',
+      routerState: RouterState.Minimal
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
