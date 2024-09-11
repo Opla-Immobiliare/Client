61b20e4cb9dda4971b2481c6d00047eba259feb8
klamaj <kri.lamaj@gmail.com>
2024-09-11 14:19:44 +0300

    Add Property 1st Approach

--- src/app/modules/properties/add-property/type/type.component.ts
+++ src/app/modules/properties/add-property/type/type.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-type',
   templateUrl: './type.component.html',
   styleUrls: ['./type.component.scss']
 })
-export class TypeComponent {
 
+export class TypeComponent implements OnInit {
+
+  typeForm: FormGroup;
+
+  constructor() {
+    this.typeForm = this.generateFormType();
+  }
+
+  // GenerateTypeForm
+  generateFormType(): FormGroup {
+    return new FormGroup({
+      rentOrSale: new FormControl("buy", [Validators.required]),
+      propertyType: new FormControl("1", [Validators.required]),
+      propertyCategory: new FormControl(undefined, [Validators.required]),
+      category: new FormControl("Choose category", [Validators.required]),
+      comune: new FormControl("Choose comune", [Validators.required])
+    });
+  }
+
+  ngOnInit(): void {}
 }
