77028b88bb855820c15165b074eab75e0ac829d5
klamaj <kri.lamaj@gmail.com>
2024-10-04 15:32:45 +0300

    Complete Additional Features

--- src/app/modules/properties/property/additional-features/additional-features.component.ts
+++ src/app/modules/properties/property/additional-features/additional-features.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { PropertyAdditionalFeatures } from '../../models/property-additional-features.model';
+import localeIt from '@angular/common/locales/it'
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeIt, 'it');
 
 @Component({
   selector: 'app-additional-features',
   templateUrl: './additional-features.component.html',
   styleUrls: ['./additional-features.component.scss']
 })
-export class AdditionalFeaturesComponent implements OnInit {
 
-  constructor() {}
-
-  ngOnInit(): void {}
+export class AdditionalFeaturesComponent {
+  @Input() features: PropertyAdditionalFeatures | undefined;
 }
