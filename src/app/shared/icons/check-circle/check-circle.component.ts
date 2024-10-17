d062393644417854e721b6553cdf48b09de0465c
alexlamaj <alesandrolamaj@gmail.com>
2024-10-17 18:19:03 +0300

    Thank You Page Design

--- src/app/shared/icons/check-circle/check-circle.component.ts
+++ src/app/shared/icons/check-circle/check-circle.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-check-circle',
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class CheckCircleComponent {
 
+  @Input() color:string = '';
+  @Input() dimensions:string = '';
 }
